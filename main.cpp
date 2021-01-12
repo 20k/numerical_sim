8bf725e0ca59a8fc1258271f76b22113f125800c
James <Icedshot@gmail.com>
2021-01-12 04:10:17 +0000

    this seems to calculate a conformal metric with a correct determinant

--- main.cpp
+++ main.cpp
@@ -5,6 +5,107 @@
 #include <GLFW/glfw3.h>
 #include <SFML/Graphics.hpp>
 #include <CL/cl_ext.h>
+#include <geodesic/dual.hpp>
+#include <geodesic/dual_value.hpp>
+
+///all conformal variables are explicitly labelled
+struct bssnok_data
+{
+    /**
+    conformal
+    [0, 1, 2,
+     X, 3, 4,
+     X, X, 5]
+    */
+    cl_float cY0, cY1, cY2, cY3, cY4, cY5;
+
+    /**
+    conformal
+    [0, 1, 2,
+     X, 3, 4,
+     X, X, 5]
+    */
+    cl_float cA0, cA1, cA2, cA3, cA4, cA5;
+
+    cl_float cGi0, cGi1, cGi2;
+
+    cl_float K;
+    cl_float X;
+};
+
+bssnok_data get_conditions(vec3f pos, vec3f centre, float scale)
+{
+    tensor<float, 3, 3> kronecker;
+
+    for(int i=0; i < 3; i++)
+    {
+        for(int j=0; j < 3; j++)
+        {
+            if(i == j)
+                kronecker.idx(i, j) = 1;
+            else
+                kronecker.idx(i, j) = 0;
+        }
+    }
+
+    ///I could fix this by improving the dual library to allow for algebraic substitution
+    value initial_conformal_factor = 1;
+
+    float r = (pos - centre).length() * scale;
+
+    value vr("r");
+
+    std::vector<float> black_hole_r{0};
+    std::vector<float> black_hole_m{1};
+
+    ///3.57 https://scholarworks.rit.edu/cgi/viewcontent.cgi?article=11286&context=theses
+    for(int i=0; i < (int)black_hole_r.size(); i++)
+    {
+        float Mi = black_hole_m[i];
+        float ri = black_hole_r[i];
+
+        initial_conformal_factor = initial_conformal_factor + Mi / (2 * fabs(vr - ri));
+    }
+
+    tensor<value, 3, 3> yij;
+
+    for(int i=0; i < 3; i++)
+    {
+        for(int j=0; j < 3; j++)
+        {
+            yij.idx(i, j) = pow(initial_conformal_factor, 4) * kronecker.idx(i, j);
+        }
+    }
+
+    ///https://arxiv.org/pdf/gr-qc/9810065.pdf, 11
+    value Y = yij.det();
+
+    ///https://scholarworks.rit.edu/cgi/viewcontent.cgi?article=11286&context=theses pre 3.65
+    value conformal_factor = (1/12.f) * log(Y);
+
+    tensor<value, 3, 3> cyij;
+
+    for(int i=0; i < 3; i++)
+    {
+        for(int j=0; j < 3; j++)
+        {
+            cyij.idx(i, j) = exp(-4 * conformal_factor) * yij.idx(i, j);
+        }
+    }
+
+    ///determinant of cyij is 1?
+    ///
+
+    value cY = cyij.det();
+
+    std::cout << "CY " << type_to_string(cY.substitute("r", r)) << std::endl;
+
+    float real_value = cY.substitute("r", r).get_constant();
+
+    std::cout << "REAL " << real_value << std::endl;
+
+    return bssnok_data();
+}
 
 int main()
 {
@@ -42,6 +143,8 @@ int main()
     rtex[1].create_from_texture(tex[1].handle);
 
     int which_buffer = 0;
+
+    get_conditions({0, 1, 0}, {0, 0, 0}, 1);
 
     while(!win.should_close())
     {
