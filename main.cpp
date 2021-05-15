94d124514f4c097fe4d26708e2c6de25ffa46f65
20k <icedshot@gmail.com>
2021-05-15 23:39:38 +0100

    note to self on stability

--- main.cpp
+++ main.cpp
@@ -2438,6 +2438,7 @@ void build_eqs(equation_context& ctx)
     #endif // SIMPLE_CHRISTOFFEL
 
     ///https://arxiv.org/pdf/1205.5111v1.pdf 49
+    ///made it to 58 with this
     #define CHRISTOFFEL_49
     #ifdef CHRISTOFFEL_49
     tensor<value, 3, 3> littlekij = unpinned_icY.to_tensor() * K;
