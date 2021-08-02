9a67760b96b994b93be2b2c54d5f0c3b970ae4c9
20k <icedshot@gmail.com>
2021-08-02 01:06:58 +0100

    this doesn't help

--- main.cpp
+++ main.cpp
@@ -4616,6 +4616,14 @@ int main()
             }
         }
 
+        /*if(time_elapsed_s >= 20)
+        {
+            for(auto& i : dissipation_coefficients)
+            {
+                i = std::min(i, 0.1f);
+            }
+        }*/
+
         win.poll();
 
         if(!ImGui::GetIO().WantCaptureKeyboard)
