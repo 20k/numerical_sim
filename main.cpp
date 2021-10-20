a50fb9aa3d4f58751b41361e7191111c978f1602
20k <icedshot@gmail.com>
2021-10-20 17:31:52 +0100

    -cl-fast-relaxed-math is the option which seems to specifically violate associativity, the other options are safe and give a performance boost

--- main.cpp
+++ main.cpp
@@ -4128,7 +4128,7 @@ int main()
 
     opencl_context& clctx = *win.clctx;
 
-    std::string argument_string = "-O3 -cl-std=CL2.0 -cl-uniform-work-group-size -cl-mad-enable -cl-finite-math-only ";
+    std::string argument_string = "-O3 -cl-std=CL2.0 -cl-uniform-work-group-size -cl-mad-enable -cl-finite-math-only -cl-unsafe-math-optimizations ";
 
     ///the simulation domain is this * 2
     int current_simulation_boundary = 1024;
