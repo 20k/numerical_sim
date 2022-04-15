eb34aab6a834b26b22844aa45e8a0559add8a928
20k <icedshot@gmail.com>
2022-04-15 21:46:55 +0100

    this piecewise formulation doesn't help

--- gravitational_waves.cpp
+++ gravitational_waves.cpp
@@ -124,7 +124,7 @@ auto integrate_1d(const T& func, int n, float upper, float lower)
     using variable_type = decltype(func(0.f));
     variable_type sum =  0;
 
-    int pieces = 4;
+    int pieces = 1;
     float step = (upper - lower) / pieces;
 
     for(int i=0; i < pieces; i++)
