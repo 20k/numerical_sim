934e5b965f0324832b8a05235e0872cad2b6493b
20k <icedshot@gmail.com>
2022-04-21 10:57:02 +0100

    error as function arg

--- main.cpp
+++ main.cpp
@@ -1981,6 +1981,191 @@ value calculate_conformal_guess(const tensor<value, 3>& pos, const std::vector<b
     return BL_s;
 }
 
+
+void check_symmetry(const std::string& debug_name, cl::command_queue& cqueue, cl::buffer& arg, vec<4, cl_int> size)
+{
+    //#define CHECK_SYMMETRY
+    #ifdef CHECK_SYMMETRY
+    std::cout << debug_name << std::endl;
+
+    cl::args check;
+    check.push_back(arg);
+    check.push_back(size);
+
+    cqueue.exec("check_z_symmetry", check, {size.x(), size.y(), size.z()}, {8, 8, 1});
+
+    cqueue.block();
+    #endif // CHECK_SYMMETRY
+}
+
+cl::buffer solve_for_u(cl::context& ctx, cl::command_queue& cqueue, vec<4, cl_int> base_size, float c_at_max, int scale_factor, cl::buffer& gpu_holes, std::optional<cl::buffer> base, cl_float etol)
+{
+    vec<4, cl_int> reduced_clsize = ((base_size - 1) / scale_factor) + 1;
+
+    std::array<cl::buffer, 2> reduced_u_args{ctx, ctx};
+
+    if(base.has_value())
+        reduced_u_args[0] = base.value();
+    else
+        reduced_u_args[0].alloc(reduced_clsize.x() * reduced_clsize.y() * reduced_clsize.z() * sizeof(cl_float));
+
+    reduced_u_args[1].alloc(reduced_clsize.x() * reduced_clsize.y() * reduced_clsize.z() * sizeof(cl_float));
+
+    int which_reduced = 0;
+
+    if(!base.has_value())
+    {
+        cl::args initial_u_args;
+        initial_u_args.push_back(reduced_u_args[0]);
+        initial_u_args.push_back(reduced_clsize);
+
+        cqueue.exec("setup_u_offset", initial_u_args, {reduced_clsize.x(), reduced_clsize.y(), reduced_clsize.z()}, {8, 8, 1});
+    }
+
+    cl::copy(cqueue, reduced_u_args[0], reduced_u_args[1]);
+
+    int N = 8000;
+
+    #ifdef GPU_PROFILE
+    N = 1000;
+    #endif // GPU_PROFILE
+
+    #ifdef QUICKSTART
+    N = 200;
+    #endif // QUICKSTART
+
+    //cl_int still_going = 0;
+
+    std::array<cl::buffer, 2> still_going{ctx, ctx};
+
+    cl_int one = 1;
+
+    for(int i=0; i < 2; i++)
+    {
+        still_going[i].alloc(sizeof(cl_int));
+        still_going[i].fill(cqueue, one);
+    }
+
+    int which_still_going = 0;
+
+    for(int i=0; i < N; i++)
+    {
+        float local_scale = calculate_scale(c_at_max, reduced_clsize);
+
+        cl::args iterate_u_args;
+        iterate_u_args.push_back(reduced_u_args[which_reduced]);
+        iterate_u_args.push_back(reduced_u_args[(which_reduced + 1) % 2]);
+        iterate_u_args.push_back(local_scale);
+        iterate_u_args.push_back(reduced_clsize);
+        iterate_u_args.push_back(still_going[which_still_going]);
+        iterate_u_args.push_back(still_going[(which_still_going + 1) % 2]);
+        iterate_u_args.push_back(etol);
+        iterate_u_args.push_back(gpu_holes);
+
+        cqueue.exec("iterative_u_solve", iterate_u_args, {reduced_clsize.x(), reduced_clsize.y(), reduced_clsize.z()}, {8, 8, 1});
+
+        if(((i % 50) == 0) && still_going[(which_still_going + 1) % 2].read<cl_int>(cqueue)[0] == 0)
+            break;
+
+        still_going[which_still_going].set_to_zero(cqueue);
+
+        which_reduced = (which_reduced + 1) % 2;
+        which_still_going = (which_still_going + 1) % 2;
+    }
+
+    check_symmetry("post_iterate", cqueue, reduced_u_args[which_reduced], reduced_clsize);
+
+    return reduced_u_args[which_reduced];
+}
+
+#ifdef OLD_FAST_U
+cl::buffer upscale_u(cl::context& ctx, cl::command_queue& cqueue, cl::buffer& source_buffer, vec<4, cl_int> base_size, int upscale_scale, int source_scale)
+{
+    vec<4, cl_int> reduced_clsize = ((base_size - 1) / source_scale) + 1;
+    vec<4, cl_int> upper_clsize = ((base_size - 1) / upscale_scale) + 1;
+
+    check_symmetry("pre_iterate", cqueue, source_buffer, reduced_clsize);
+
+    cl::buffer u_arg(ctx);
+    u_arg.alloc(upper_clsize.x() * upper_clsize.y() * upper_clsize.z() * sizeof(cl_float));
+
+    cl::args upscale_args;
+    upscale_args.push_back(source_buffer);
+    upscale_args.push_back(u_arg);
+    upscale_args.push_back(reduced_clsize);
+    upscale_args.push_back(upper_clsize);
+
+    cqueue.exec("upscale_u", upscale_args, {upper_clsize.x(), upper_clsize.y(), upper_clsize.z()}, {8, 8, 1});
+
+    check_symmetry("post_upscale", cqueue, u_arg, upper_clsize);
+
+    return u_arg;
+}
+
+cl::buffer iterate_u(cl::context& ctx, cl::command_queue& cqueue, vec3i size, float c_at_max)
+{
+    vec<4, cl_int> clsize = {size.x(), size.y(), size.z(), 0};
+
+    std::optional<cl::buffer> last;
+
+    for(int i=2; i >= 0; i--)
+    {
+        int up_size = pow(2, i+1);
+        int current_size = pow(2, i);
+
+        cl::buffer reduced = solve_for_u(ctx, cqueue, clsize, c_at_max, up_size, last);
+
+        cl::buffer upscaled = upscale_u(ctx, cqueue, reduced, clsize, current_size, up_size);
+
+        last = upscaled;
+    }
+
+    return solve_for_u(ctx, cqueue, clsize, c_at_max, 1, last);
+}
+#endif // OLD_FAST_U
+
+cl::buffer extract_u_region(cl::context& ctx, cl::command_queue& cqueue, cl::buffer& in, float c_at_max_in, float c_at_max_out, vec<4, cl_int> clsize)
+{
+    cl::buffer out(ctx);
+    out.alloc(in.alloc_size);
+
+    cl::args upscale_args;
+    upscale_args.push_back(in);
+    upscale_args.push_back(out);
+    upscale_args.push_back(c_at_max_in);
+    upscale_args.push_back(c_at_max_out);
+    upscale_args.push_back(clsize);
+
+    cqueue.exec("extract_u_region", upscale_args, {clsize.x(), clsize.y(), clsize.z()}, {8, 8, 1});
+
+    check_symmetry("extract_u_region", cqueue, out, clsize);
+
+    return out;
+}
+
+cl::buffer iterate_u(cl::context& ctx, cl::command_queue& cqueue, vec3i size, float c_at_max, cl::buffer& gpu_holes, cl_float etol)
+{
+    float boundaries[4] = {c_at_max, c_at_max * 4, c_at_max * 8, c_at_max * 16};
+
+    vec<4, cl_int> clsize = {size.x(), size.y(), size.z(), 0};
+
+    std::optional<cl::buffer> last;
+
+    for(int i=2; i >= 0; i--)
+    {
+        float current_boundary = boundaries[i + 1];
+        float next_boundary = boundaries[i];
+
+        cl::buffer reduced = solve_for_u(ctx, cqueue, clsize, current_boundary, 1, gpu_holes, last, etol);
+
+        cl::buffer extracted = extract_u_region(ctx, cqueue, reduced, current_boundary, next_boundary, clsize);
+
+        last = extracted;
+    }
+
+    return solve_for_u(ctx, cqueue, clsize, c_at_max, 1, gpu_holes, last, etol);
+}
+
 inline
 initial_conditions setup_initial_conditions(cl::context& clctx, cl::command_queue& cqueue, equation_context& ctx, vec3f centre, float scale)
 {
@@ -4404,191 +4589,6 @@ struct lightray
     cl_int x, y;
 };
 
-void check_symmetry(const std::string& debug_name, cl::command_queue& cqueue, cl::buffer& arg, vec<4, cl_int> size)
-{
-    //#define CHECK_SYMMETRY
-    #ifdef CHECK_SYMMETRY
-    std::cout << debug_name << std::endl;
-
-    cl::args check;
-    check.push_back(arg);
-    check.push_back(size);
-
-    cqueue.exec("check_z_symmetry", check, {size.x(), size.y(), size.z()}, {8, 8, 1});
-
-    cqueue.block();
-    #endif // CHECK_SYMMETRY
-}
-
-cl::buffer solve_for_u(cl::context& ctx, cl::command_queue& cqueue, vec<4, cl_int> base_size, float c_at_max, int scale_factor, cl::buffer& gpu_holes, std::optional<cl::buffer> base)
-{
-    vec<4, cl_int> reduced_clsize = ((base_size - 1) / scale_factor) + 1;
-
-    std::array<cl::buffer, 2> reduced_u_args{ctx, ctx};
-
-    if(base.has_value())
-        reduced_u_args[0] = base.value();
-    else
-        reduced_u_args[0].alloc(reduced_clsize.x() * reduced_clsize.y() * reduced_clsize.z() * sizeof(cl_float));
-
-    reduced_u_args[1].alloc(reduced_clsize.x() * reduced_clsize.y() * reduced_clsize.z() * sizeof(cl_float));
-
-    int which_reduced = 0;
-
-    if(!base.has_value())
-    {
-        cl::args initial_u_args;
-        initial_u_args.push_back(reduced_u_args[0]);
-        initial_u_args.push_back(reduced_clsize);
-
-        cqueue.exec("setup_u_offset", initial_u_args, {reduced_clsize.x(), reduced_clsize.y(), reduced_clsize.z()}, {8, 8, 1});
-    }
-
-    cl::copy(cqueue, reduced_u_args[0], reduced_u_args[1]);
-
-    int N = 8000;
-
-    #ifdef GPU_PROFILE
-    N = 1000;
-    #endif // GPU_PROFILE
-
-    #ifdef QUICKSTART
-    N = 200;
-    #endif // QUICKSTART
-
-    //cl_int still_going = 0;
-
-    std::array<cl::buffer, 2> still_going{ctx, ctx};
-
-    cl_int one = 1;
-
-    for(int i=0; i < 2; i++)
-    {
-        still_going[i].alloc(sizeof(cl_int));
-        still_going[i].fill(cqueue, one);
-    }
-
-    int which_still_going = 0;
-
-    cl_float etol = 0.000001f;
-
-    for(int i=0; i < N; i++)
-    {
-        float local_scale = calculate_scale(c_at_max, reduced_clsize);
-
-        cl::args iterate_u_args;
-        iterate_u_args.push_back(reduced_u_args[which_reduced]);
-        iterate_u_args.push_back(reduced_u_args[(which_reduced + 1) % 2]);
-        iterate_u_args.push_back(local_scale);
-        iterate_u_args.push_back(reduced_clsize);
-        iterate_u_args.push_back(still_going[which_still_going]);
-        iterate_u_args.push_back(still_going[(which_still_going + 1) % 2]);
-        iterate_u_args.push_back(etol);
-        iterate_u_args.push_back(gpu_holes);
-
-        cqueue.exec("iterative_u_solve", iterate_u_args, {reduced_clsize.x(), reduced_clsize.y(), reduced_clsize.z()}, {8, 8, 1});
-
-        if(((i % 50) == 0) && still_going[(which_still_going + 1) % 2].read<cl_int>(cqueue)[0] == 0)
-            break;
-
-        still_going[which_still_going].set_to_zero(cqueue);
-
-        which_reduced = (which_reduced + 1) % 2;
-        which_still_going = (which_still_going + 1) % 2;
-    }
-
-    check_symmetry("post_iterate", cqueue, reduced_u_args[which_reduced], reduced_clsize);
-
-    return reduced_u_args[which_reduced];
-}
-#ifdef OLD_FAST_U
-cl::buffer upscale_u(cl::context& ctx, cl::command_queue& cqueue, cl::buffer& source_buffer, vec<4, cl_int> base_size, int upscale_scale, int source_scale)
-{
-    vec<4, cl_int> reduced_clsize = ((base_size - 1) / source_scale) + 1;
-    vec<4, cl_int> upper_clsize = ((base_size - 1) / upscale_scale) + 1;
-
-    check_symmetry("pre_iterate", cqueue, source_buffer, reduced_clsize);
-
-    cl::buffer u_arg(ctx);
-    u_arg.alloc(upper_clsize.x() * upper_clsize.y() * upper_clsize.z() * sizeof(cl_float));
-
-    cl::args upscale_args;
-    upscale_args.push_back(source_buffer);
-    upscale_args.push_back(u_arg);
-    upscale_args.push_back(reduced_clsize);
-    upscale_args.push_back(upper_clsize);
-
-    cqueue.exec("upscale_u", upscale_args, {upper_clsize.x(), upper_clsize.y(), upper_clsize.z()}, {8, 8, 1});
-
-    check_symmetry("post_upscale", cqueue, u_arg, upper_clsize);
-
-    return u_arg;
-}
-
-cl::buffer iterate_u(cl::context& ctx, cl::command_queue& cqueue, vec3i size, float c_at_max)
-{
-    vec<4, cl_int> clsize = {size.x(), size.y(), size.z(), 0};
-
-    std::optional<cl::buffer> last;
-
-    for(int i=2; i >= 0; i--)
-    {
-        int up_size = pow(2, i+1);
-        int current_size = pow(2, i);
-
-        cl::buffer reduced = solve_for_u(ctx, cqueue, clsize, c_at_max, up_size, last);
-
-        cl::buffer upscaled = upscale_u(ctx, cqueue, reduced, clsize, current_size, up_size);
-
-        last = upscaled;
-    }
-
-    return solve_for_u(ctx, cqueue, clsize, c_at_max, 1, last);
-}
-#endif // OLD_FAST_U
-
-cl::buffer extract_u_region(cl::context& ctx, cl::command_queue& cqueue, cl::buffer& in, float c_at_max_in, float c_at_max_out, vec<4, cl_int> clsize)
-{
-    cl::buffer out(ctx);
-    out.alloc(in.alloc_size);
-
-    cl::args upscale_args;
-    upscale_args.push_back(in);
-    upscale_args.push_back(out);
-    upscale_args.push_back(c_at_max_in);
-    upscale_args.push_back(c_at_max_out);
-    upscale_args.push_back(clsize);
-
-    cqueue.exec("extract_u_region", upscale_args, {clsize.x(), clsize.y(), clsize.z()}, {8, 8, 1});
-
-    check_symmetry("extract_u_region", cqueue, out, clsize);
-
-    return out;
-}
-
-cl::buffer iterate_u(cl::context& ctx, cl::command_queue& cqueue, vec3i size, float c_at_max, cl::buffer& gpu_holes)
-{
-    float boundaries[4] = {c_at_max, c_at_max * 4, c_at_max * 8, c_at_max * 16};
-
-    vec<4, cl_int> clsize = {size.x(), size.y(), size.z(), 0};
-
-    std::optional<cl::buffer> last;
-
-    for(int i=2; i >= 0; i--)
-    {
-        float current_boundary = boundaries[i + 1];
-        float next_boundary = boundaries[i];
-
-        cl::buffer reduced = solve_for_u(ctx, cqueue, clsize, current_boundary, 1, gpu_holes, last);
-
-        cl::buffer extracted = extract_u_region(ctx, cqueue, reduced, current_boundary, next_boundary, clsize);
-
-        last = extracted;
-    }
-
-    return solve_for_u(ctx, cqueue, clsize, c_at_max, 1, gpu_holes, last);
-}
-
 ///it seems like basically i need numerical dissipation of some form
 ///if i didn't evolve where sponge = 1, would be massively faster
 int main()
@@ -4663,7 +4663,7 @@ int main()
     {
         cl::command_queue cqueue(clctx.ctx);
 
-        u_arg = iterate_u(clctx.ctx, cqueue, size, c_at_max, gpu_holes).as_read_only();
+        u_arg = iterate_u(clctx.ctx, cqueue, size, c_at_max, gpu_holes, 0.000001f).as_read_only();
 
         cqueue.block();
     };
