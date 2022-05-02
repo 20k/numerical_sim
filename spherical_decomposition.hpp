49c9912e62e6d69fc02fea064baa75b0561df80b
20k <icedshot@gmail.com>
2022-05-02 15:41:49 +0100

    showing some quite bad errors

--- spherical_decomposition.hpp
+++ spherical_decomposition.hpp
@@ -39,18 +39,49 @@ void test_spherical_decomp()
 {
     ///http://scipp.ucsc.edu/~haber/ph116C/SphericalHarmonics_12.pdf 12
     {
+        float tol = 0.001f;
+
         auto my_real_function = [](vec3f pos)
         {
-            return pos.x() * sin(pos.y()) * cos(pos.z());
+            return pos.x() * sin(pos.y()) * cos(pos.z()) + pos.x() + pos.y() + pos.z();
         };
 
         std::map<int, std::map<int, float>> lm;
+
+        float radius = 1;
 
         for(int l=0; l < 10; l++)
         {
             for(int m=-l; m <= l; m++)
             {
-                lm[l][m] = spherical_decompose_complex_cartesian_function(my_real_function,  0, l, m, {0,0,0}, 1.f, 64).real;
+                lm[l][m] = spherical_decompose_complex_cartesian_function(my_real_function,  0, l, m, {0,0,0}, radius, 64).real;
+            }
+        }
+
+        for(float theta = 0; theta <= M_PI; theta += M_PI/8)
+        {
+            for(float phi = 0; phi <= 2 * M_PI; phi += M_PI/16)
+            {
+                vec3f cart = {radius * cos(phi) * sin(theta), radius * sin(phi) * sin(theta), radius * cos(theta)};
+
+                float my_function = my_real_function(cart);
+
+                dual_types::complex<float> sum = {0,0};
+
+                for(int l=0; l < 10; l++)
+                {
+                    for(int m=-l; m <= l; m++)
+                    {
+                        sum += lm[l][m] * sYlm_2(0, l, m, theta, phi);
+                    }
+                }
+
+                printf("Theta phi %f %f Cart %f %f %f\n", theta, phi, cart.x(), cart.y(), cart.z());
+
+                printf("Sum real %.20f\n", sum.real);
+                printf("My function %.20f\n", my_function);
+
+                assert(fabs(sum.real - my_function) < tol);
             }
         }
     }
