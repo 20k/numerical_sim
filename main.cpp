59f2b8452a4a1f749cc289612554951fe8c84736
20k <icedshot@gmail.com>
2022-05-07 21:30:52 +0100

    notes on post newtonian

--- main.cpp
+++ main.cpp
@@ -102,7 +102,7 @@ https://arxiv.org/pdf/gr-qc/0003101.pdf - fat hydrodynamic paper
 https://arxiv.org/pdf/1606.04881.pdf - this provides mixed neutron star black hole initial conditions
 https://www.hindawi.com/journals/aa/2017/6127031/ - neutron star initial conditions
 https://www.aanda.org/articles/aa/pdf/2010/06/aa12738-09.pdf - this gives exact formulas for pressure and density
-https://gwic.ligo.org/assets/docs/theses/Read_Thesis.pdf - 3.4 definition of e
+https://gwic.ligo.org/assets/docs/theses/Read_Thesis.pdf - 3.4 definition of e. This also contains some interesting post newtonian expansions
 https://arxiv.org/pdf/gr-qc/0403029.pdf - 2.13 definition of e. I don't think they're the same as the other e, but I think this is fixable
 */
 
