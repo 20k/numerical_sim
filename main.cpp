39329eee9bf2578ab7b3eea283e467a32e9ec530
20k <icedshot@gmail.com>
2022-05-08 14:21:21 +0100

    begin testing with actual neutron star, nans terribly

--- main.cpp
+++ main.cpp
@@ -2826,7 +2826,7 @@ initial_conditions setup_dynamic_initial_conditions(cl::context& clctx, cl::comm
     #define PAPER_0610128
     #ifdef PAPER_0610128
     compact_object::data<float> h1;
-    h1.t = compact_object::BLACK_HOLE;
+    h1.t = compact_object::NEUTRON_STAR;
     h1.bare_mass = 0.483;
     h1.momentum = {0, 0.133 * 0.8, 0};
     h1.position = {-3.257, 0.f, 0.f};
