4315d3af56573ef4ab71edae4458f62e51709cf3
20k <icedshot@gmail.com>
2022-05-08 19:05:05 +0100

    notes on tov

--- main.cpp
+++ main.cpp
@@ -2604,6 +2604,8 @@ laplace_data setup_u_laplace(cl::context& clctx, const std::vector<compact_objec
     //assert(cpu_holes[0].t == compact_object::NEUTRON_STAR);
 
     ///https://arxiv.org/pdf/1606.04881.pdf I think I need to do (85)
+    ///ok no: I think what it is is that they're solving for ph in ToV, which uses tov's conformally flat variable
+    ///whereas I'm getting values directly out of an analytic solution
     value U_RHS = (-1.f/8.f) * aij_aIJ_dyn * pow(phi, -7) - 2 * M_PI * pow(phi, -3) * ppw2p;
 
     /*tensor<value, 3> vpos = {cpu_holes[0].position.x(), cpu_holes[0].position.y(), cpu_holes[0].position.z()};
