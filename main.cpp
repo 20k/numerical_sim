d5355d30f0e7837ee9de5266a45a1ee6902ae2c2
20k <icedshot@gmail.com>
2022-05-19 03:23:30 +0100

    why doesn't this work?

--- main.cpp
+++ main.cpp
@@ -3671,13 +3671,13 @@ initial_conditions setup_dynamic_initial_conditions(cl::context& clctx, cl::comm
     h1.t = compact_object::NEUTRON_STAR;
     h1.bare_mass = 0.1;
     h1.momentum = {0, 0.133 * 0.8 * 0.1, 0};
-    h1.position = {-4.257, 0.f, 0.f};
+    h1.position = {-3.257, 0.f, 0.f};
 
     compact_object::data<float> h2;
     h2.t = compact_object::NEUTRON_STAR;
     h2.bare_mass = 0.1;
     h2.momentum = {0, -0.133 * 0.8 * 0.1, 0};
-    h2.position = {4.257, 0.f, 0.f};
+    h2.position = {3.257, 0.f, 0.f};
 
     objects.push_back(h1);
     objects.push_back(h2);
