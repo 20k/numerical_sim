9909ecead47aabd7ceea43b9928a1a42bc36771e
20k <icedshot@gmail.com>
2022-05-21 22:00:54 +0100

    This case detonates when it absolutely shouldn't

--- main.cpp
+++ main.cpp
@@ -3685,15 +3685,15 @@ initial_conditions setup_dynamic_initial_conditions(cl::context& clctx, cl::comm
     #define PAPER_0610128
     #ifdef PAPER_0610128
     compact_object::data<float> h1;
-    h1.t = compact_object::NEUTRON_STAR;
+    h1.t = compact_object::BLACK_HOLE;
     h1.bare_mass = 0.15;
-    h1.momentum = {0, 0.133 * 0.8 * 0.5, 0};
+    h1.momentum = {0, 0.133 * 0.8 * 0.5 * 0, 0};
     h1.position = {-4.257, 0.f, 0.f};
 
     compact_object::data<float> h2;
-    h2.t = compact_object::NEUTRON_STAR;
+    h2.t = compact_object::BLACK_HOLE;
     h2.bare_mass = 0.15;
-    h2.momentum = {0, -0.133 * 0.8 * 0.5, 0};
+    h2.momentum = {0, -0.133 * 0.8 * 0.5 * 0, 0};
     h2.position = {4.257, 0.f, 0.f};
 
     objects.push_back(h1);
