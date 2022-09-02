b68b8361bf611ce60ae6da8c6350cc98e4b1effc
20k <icedshot@gmail.com>
2022-09-02 06:30:01 +0100

    cleanup old code

--- laplace_solver.hpp
+++ laplace_solver.hpp
@@ -46,38 +46,4 @@ struct sandwich_result
 
 sandwich_result sandwich_solver(cl::context& clcltx, cl::command_queue& cqueue, const sandwich_data& data, float scale, vec3i dim, float err = 0.0001f);
 
-struct tov_input
-{
-    value phi_rhs;
-    value u_to_phi;
-    std::vector<std::pair<std::string, value>> extras;
-};
-
-struct tov_solver
-{
-    tov_solver(cl::context& ctx) : phi(ctx){}
-
-    cl::buffer phi;
-};
-
-tov_solver tov_solve(cl::context& clctx, cl::command_queue& cqueue, const tov_input& solve, float scale, vec3i dim, float err = 0.0001f);
-
-struct cache_args
-{
-    cl::buffer tov_phi;
-    cl::buffer aij_aIJ;
-    cl::buffer ppw2p;
-    std::array<cl::buffer, 6> bcAij;
-    cl::buffer superimposed_tov_phi;
-
-    value aij_aIJ_eq;
-    value ppw2p_eq;
-    std::array<value, 6> bcAij_eq;
-    value superimposed_tov_phi_eq;
-
-    cache_args(cl::context& ctx) : tov_phi(ctx), aij_aIJ(ctx), ppw2p(ctx), bcAij{ctx, ctx, ctx, ctx, ctx, ctx}, superimposed_tov_phi(ctx) {}
-};
-
-void update_cached_variables(cl::context& clctx, cl::command_queue& cqueue, equation_context& cache, cache_args& args, float scale, vec3i dim);
-
 #endif // LAPLACE_SOLVER_HPP_INCLUDED
