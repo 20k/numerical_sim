a27f2ec106bec4401fca384f339cb4e01f8858d0
20k <icedshot@gmail.com>
2022-09-14 06:43:05 +0100

    add the bad cloud case

--- main.cpp
+++ main.cpp
@@ -4075,6 +4075,22 @@ initial_conditions setup_dynamic_initial_conditions(cl::context& clctx, cl::comm
     objects = {h1, h2};
     #endif // NEUTRON_BLACK_HOLE_MERGE
 
+    ///defined for a compactness of 0.02 sigh
+    //#define GAS_CLOUD_BLACK_HOLE
+    #ifdef GAS_CLOUD_BLACK_HOLE
+    compact_object::data h1;
+    h1.t = compact_object::BLACK_HOLE;
+    h1.bare_mass = 0.5;
+    h1.momentum = {0, 0.133 * 0.8 * 0.02, 0};
+    h1.position = {-5.257, 3.f, 0.f};
+
+    compact_object::data h2;
+    h2.t = compact_object::NEUTRON_STAR;
+    h2.bare_mass = 0.09;
+    h2.momentum = {0, -0.133 * 0.8 * 0.20, 0};
+    h2.position = {4.257, 3.f, 0.f};
+    #endif // GAS_CLOUD_BLACK_HOLE
+
     //#define REGULAR_MERGE
     #ifdef REGULAR_MERGE
     compact_object::data h1;
