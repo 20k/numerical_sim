ed4f44bf1725b65b9511b1f61815cd282d4a7949
20k <icedshot@gmail.com>
2022-10-29 11:21:57 +0100

    generate particle positions

--- particle_dynamics.cpp
+++ particle_dynamics.cpp
@@ -38,6 +38,37 @@ void particle_dynamics::init(cpu_mesh& mesh, cl::context& ctx, cl::command_queue
         particle_3_position[i].alloc(sizeof(cl_float) * 3 * particle_num);
         particle_3_velocity[i].alloc(sizeof(cl_float) * 3 * particle_num);
     }
+
+    float generation_radius = 0.5f * get_c_at_max()/2.f;
+
+    ///need to use an actual rng if i'm doing anything even vaguely scientific
+    std::minstd_rand0 rng(1234);
+
+    std::vector<vec3f> positions;
+
+    for(int i=0; i < particle_num; i++)
+    {
+        int kk=0;
+
+        for(; kk < 1024; kk++)
+        {
+            float x = rand_det_s(rng, -1.f, 1.f) * generation_radius;
+            float y = rand_det_s(rng, -1.f, 1.f) * generation_radius;
+            float z = rand_det_s(rng, -1.f, 1.f) * generation_radius;
+
+            vec3f pos = {x, y, z};
+
+            if(pos.length() >= generation_radius)
+                continue;
+
+            positions.push_back(pos);
+        }
+
+        if(kk == 1024)
+            throw std::runtime_error("Did not successfully assign particle position");
+    }
+
+    assert((int)positions.size() == particle_num);
 }
 
 void particle_dynamics::step(cpu_mesh& mesh, cl::context& ctx, cl::managed_command_queue& mqueue, thin_intermediates_pool& pool, buffer_set& in, buffer_set& out, buffer_set& base, float timestep, int iteration, int max_iteration)
