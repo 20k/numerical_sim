14970fd3a821bdaf4f2c9219cecf2a3833f885c3
20k <icedshot@gmail.com>
2022-11-24 04:51:03 +0000

    notes on recovering from conformal factor

--- bssn.cpp
+++ bssn.cpp
@@ -21,6 +21,7 @@ void bssn::init(equation_context& ctx, const metric<value, 3, 3>& Yij, const ten
     //value X = exp(-4 * conformal_factor);
 
     #ifndef USE_W
+    ///X also eq (conformal + u)^-4, aka psi^-4
     value X = pow(Y, -1.f/3.f);
 
     tensor<value, 3, 3> cAij = X * Aij;
