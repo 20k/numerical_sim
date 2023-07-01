00c461454a8b03c240126ba652d6d6f66999e602
20k <icedshot@gmail.com>
2023-07-01 02:48:39 +0100

    no halfs other than as intermediate storage

--- bssn.hpp
+++ bssn.hpp
@@ -14,7 +14,7 @@
 
 template<typename T, int N>
 inline
-literal<T> buffer_read_linear_f(equation_context& ctx, buffer<T, N> buf, literal<v3f> position, literal<v4i> dim)
+literal<value> buffer_read_linear_f(equation_context& ctx, buffer<T, N> buf, literal<v3f> position, literal<v4i> dim)
 {
     ctx.order = 1;
     ctx.uses_linear = true;
@@ -24,12 +24,12 @@ literal<T> buffer_read_linear_f(equation_context& ctx, buffer<T, N> buf, literal
 
     ctx.exec(return_v(buffer_read_linear(buf, ipos, idim)));
 
-    return literal<T>();
+    return literal<value>();
 }
 
 template<typename T, int N>
 inline
-literal<T> buffer_read_linear_f4(equation_context& ctx, buffer<T, N> buf, literal<v4f> position, literal<v4i> dim)
+literal<value> buffer_read_linear_f4(equation_context& ctx, buffer<T, N> buf, literal<v4f> position, literal<v4i> dim)
 {
     ctx.order = 1;
     ctx.uses_linear = true;
@@ -39,18 +39,18 @@ literal<T> buffer_read_linear_f4(equation_context& ctx, buffer<T, N> buf, litera
 
     ctx.exec(return_v(buffer_read_linear(buf, ipos, idim)));
 
-    return literal<T>();
+    return literal<value>();
 }
 
 template<typename T, int N>
 inline
-literal<T> buffer_index_f(equation_context& ctx, buffer<T, N>& buf, literal<value_i> x, literal<value_i> y, literal<value_i> z, literal<v4i> dim)
+literal<value> buffer_index_f(equation_context& ctx, buffer<T, N>& buf, literal<value_i> x, literal<value_i> y, literal<value_i> z, literal<v4i> dim)
 {
-    T v = buf[z.get() * dim.get().x() * dim.get().y() + y.get() * dim.get().x() + x.get()];
+    value v = (value)buf[z.get() * dim.get().x() * dim.get().y() + y.get() * dim.get().x() + x.get()];
 
     ctx.exec(return_v(v));
 
-    return literal<T>();
+    return literal<value>();
 }
 
 inline
