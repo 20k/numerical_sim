c791b4c60ea34c0415b710557eecddec8ef15a84
20k <icedshot@gmail.com>
2024-08-08 18:45:56 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -2608,13 +2608,13 @@ initial_conditions setup_dynamic_initial_conditions(cl::context& clctx, cl::comm
     #ifdef PAPER_0610128
     compact_object::data h1;
     h1.t = compact_object::BLACK_HOLE;
-    h1.bare_mass = 0.5;
+    h1.bare_mass = 0.483;
     h1.momentum = {0, 0.133, 0};
     h1.position = {-3.257, 0.f, 0.f};
 
     compact_object::data h2;
     h2.t = compact_object::BLACK_HOLE;
-    h2.bare_mass = 0.5;
+    h2.bare_mass = 0.483;
     h2.momentum = {0, -0.133, 0};
     h2.position = {3.257, 0.f, 0.f};
 
